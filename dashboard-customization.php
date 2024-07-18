5c6b432b3394d8d9bcb46644579a43e87ef6cb99
awaisp-irfan <irfanmaqsood.im123@gmail.com>
2024-07-19 01:21:28 +0500

    new collaborator is added

--- dashboard-customization.php
+++ dashboard-customization.php
@@ -23,7 +23,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       dashboard-customization
  * Domain Path:       /languages
- * 
+ * Collaborator:       Ali Akbar
  */
 
 // If this file is called directly, abort.
